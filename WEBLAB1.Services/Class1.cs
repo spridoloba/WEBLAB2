9f19267198b35a1ba7211988ea63e14aea74a766
spridoloba <147507734+spridoloba@users.noreply.github.com>
2023-10-10 14:59:32 +0300

    Add files via upload

---
+++ WEBLAB1.Services/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace WEBLAB1.Services
+{
+    public interface IEmailSender
+    {
+        void SendEmail(string to, string subject, string body);
+    }
+}
---
+++ WEBLAB1.Services/MailKitEmailSender.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MailKit.Net.Smtp;
+using MimeKit;
+
+namespace WEBLAB1.Services
+{
+    public class MailKitEmailSender : IEmailSender
+    {
+        private readonly string _smtpHost;
+        private readonly int _smtpPort;
+        private readonly string _smtpUser;
+        private readonly string _smtpPass;
+
+        public MailKitEmailSender(string smtpHost, int smtpPort, string smtpUser, string smtpPass)
+        {
+            _smtpHost = smtpHost;
+            _smtpPort = smtpPort;
+            _smtpUser = smtpUser;
+            _smtpPass = smtpPass;
+        }
+
+        public void SendEmail(string to, string subject, string body)
+        {
+            var emailMessage = new MimeMessage();
+            emailMessage.From.Add(new MailboxAddress("імя", "email@khnu.com"));
+            emailMessage.To.Add(new MailboxAddress("", to));
+            emailMessage.Subject = subject;
+            emailMessage.Body = new TextPart("123") { Text = body };
+
+            using (var client = new SmtpClient())
+            {
+                client.Connect(_smtpHost, _smtpPort, MailKit.Security.SecureSocketOptions.StartTls);
+                client.Authenticate(_smtpUser, _smtpPass);
+                client.Send(emailMessage);
+                client.Disconnect(true);
+            }
+        }
+    }
+}
